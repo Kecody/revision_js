e9080011567a07dd00736a0e9f9b9e53bfea085b
Antoine07 <antoine.lucsko@gmail.com>
2021-11-24 17:33:50 +0100

    coach

--- COACH/rot.js
+++ COACH/rot.js
@@ -15,3 +15,13 @@ const letters_str = letters.join('');
 const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Essaye de retirer les lettres qui se trouvent dans letters dans alphabets
+const sanitize = [] ;
+
+for(const ch of alphabets){
+    if(letters.includes(ch)) continue ; // on passe à l'itération suivante
+
+    // si la lettre n'est pas présente on ne la garde pas
+    sanitize.push(ch);
+}
+
+document.write(`<h3>Alphabet crypté : ${letters + sanitize.join('')}</h3>`);
