6a53e931914eb9b17ee91fd933882b15237e8cb7
Antoine07 <antoine.lucsko@gmail.com>
2021-12-06 16:34:32 +0100

    condition

--- PHP/Introduction/condition.php
+++ PHP/Introduction/condition.php
@@ -34,3 +34,11 @@ $exp8 = ((false || false) && (!false && false)) || true ;
 $exp9 = (false || false) && (!false && false) || true ;
 // false && false || true
 // false || true = true
+
+$exp10 = 3*3.5 > 10 ;
+$exp11 = 3*7  == 21 ;
+$exp12 = 3-1 >= 1;
+$exp13 = 0 < pow(2,10) == pow(2,10);
+$exp14 = !(!true);
+$exp15 = (5.5*2 == 11 || 1/2 != .5) && (3%2 == 0);
+$exp16 = (5.5*2 == 11 || 1/2 != .5) && (3%2 != 0);
