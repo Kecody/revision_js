a5c04a353d44f4ea117de033e1637e64f79ef024
Antoine07 <antoine.lucsko@gmail.com>
2021-12-08 17:36:45 +0100

    exo

--- PHP/Introduction/inverse.php
+++ PHP/Introduction/inverse.php
@@ -1,8 +1,24 @@
 <?php
 
-$message = "Bonjour" ;
+$message = "Bonjour";
 
 /**
+ * Exercice 1
  * à l'aide d'un script inverser la chaîne de caractères ci-dessus
  * On peut parcourir une chaîne comme un tableau
+ */
+
+$reverse = '';
+for ($i = 0; $i < strlen($message); $i++) {
+    $reverse = $message[$i] . $reverse;
+}
+
+echo PHP_EOL;
+echo $reverse;
+echo PHP_EOL;
+
+ // Exercice 2
+ /*
+    soit le message suivant $message = "Bonjour";
+    Mettez tous les caractères dans un tableau $characters
  */
