f65f10beefe4e2dd758efa6e0df610f183595337
Antoine07 <antoine.lucsko@gmail.com>
2021-12-13 17:17:05 +0100

    calcul

--- PHP/Object/Calcul.php
+++ PHP/Object/Calcul.php
@@ -5,11 +5,11 @@ class Calcul{
     private $result = 0;
 
     public function add($num1, $num2){
-
+        // compléter
     }
 
     public function getResult(){
-
+        // compléter
     }
 }
 
@@ -18,3 +18,5 @@ class Calcul{
 Exercice écrire le code utile dans la méthode add qui ajoute deux valeurs à la variable  de classe $result. La méthode getResult affiche la valeur calculé par la méthode add.
 
 */
+
+$calculatrice = new Calcul();
