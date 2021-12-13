8ae316b3fe46825f7ecf31174460accd047d9780
Antoine07 <antoine.lucsko@gmail.com>
2021-12-13 17:16:22 +0100

    calcul

---
+++ PHP/Object/Calcul.php
@@ -0,0 +1,20 @@
+<?php
+
+class Calcul{
+
+    private $result = 0;
+
+    public function add($num1, $num2){
+
+    }
+
+    public function getResult(){
+
+    }
+}
+
+/*
+
+Exercice écrire le code utile dans la méthode add qui ajoute deux valeurs à la variable  de classe $result. La méthode getResult affiche la valeur calculé par la méthode add.
+
+*/
--- PHP/Object/Product.php
+++ PHP/Object/Product.php
@@ -9,19 +9,28 @@ class Product{
     }
 
     public function setPriceTTC($tva){
-        // mettre le prix TCC
+        
+        $this->price = $tva * $this->price + $this->price;
     }
 
     public function getPrice(){
-        // affiche le prix TTC ou pas 
+        
+        return $this->price;
     }
 }
 
 
-$apple = new Product(0.5);
 
 /*
 Exercice Créez un produit (c'est fait) et 
 1. définir une tva pour le calcul TTC du prix
 2. Affichez le prix TTC
 */
+$apple = new Product(0.5);
+
+// On passe une valeur en paramètre
+$apple->setPriceTTC(0.2);
+
+// on accède à une valeur dans le script courant
+echo $apple->getPrice();
+echo PHP_EOL;
