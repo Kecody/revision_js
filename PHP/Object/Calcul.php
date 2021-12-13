c2ecf4b2f9189b6fa769168f34a6d68a417bd568
Antoine07 <antoine.lucsko@gmail.com>
2021-12-13 17:41:18 +0100

    calculatrice sum

--- PHP/Object/Calcul.php
+++ PHP/Object/Calcul.php
@@ -5,18 +5,30 @@ class Calcul{
     private $result = 0;
 
     public function add($num1, $num2){
-        // compléter
+        $this->result = $num1 + $num2;
     }
 
     public function getResult(){
-        // compléter
+        
+        return $this->result;
+    }
+
+    public function sum($numbers){
+
     }
 }
 
 /*
 
-Exercice écrire le code utile dans la méthode add qui ajoute deux valeurs à la variable  de classe $result. La méthode getResult affiche la valeur calculé par la méthode add.
+Exercice 
 
+1. écrire le code utile dans la méthode add qui ajoute deux valeurs à la variable  de classe $result. La méthode getResult affiche la valeur calculé par la méthode add.
+2. Implémentez (faire le code) de la méthode sum de la classe Calcul elle fera la somme des valeurs d'un tableau, le résultat sera assigner à notre variable classe $result. La méthode getResult continuera à afficher le resultat.
 */
 
 $calculatrice = new Calcul();
+
+$calculatrice->add(1,2);
+
+echo $calculatrice->getResult();
+echo PHP_EOL;
