f27da9e90d0e29ea1e27e433ce60e61e11e27a6b
Antoine07 <antoine.lucsko@gmail.com>
2021-12-15 17:05:49 +0100

    calcul

--- PHP/Object/Calcul.php
+++ PHP/Object/Calcul.php
@@ -1,22 +1,47 @@
 <?php
 
-class Calcul{
+class Calcul
+{
 
     private $result = 0;
 
-    public function add($num1, $num2){
+    public function add($num1, $num2)
+    {
         $this->result = $num1 + $num2;
     }
 
-    public function getResult(){
-        
+    public function getResult()
+    {
+
         return $this->result;
     }
 
-    public function sum($numbers){
-        foreach($numbers as $num){
+    public function sum($numbers)
+    {
+        foreach ($numbers as $num) {
             $this->result += $num;
         }
+    }
+
+    public function mults($numbers)
+    {
+        $this->result = 1; // avez vous pensé à mettre cette valeur à 1
+        foreach ($numbers as $num) {
+            $this->result *= $num;
+        }
+    }
+
+    public function divide($a, $b)
+    {
+        $this->result = 1;
+
+        if ($b == 0) return false;
+
+        return round($a / $b, 2);
+    }
+
+    public function reset(){
+        $this->result = 0;
     }
 }
 
@@ -32,14 +57,28 @@ elle fera la somme des valeurs numériques d'un tableau, le résultat sera assig
 3. Faites la méthode mults qui multiplie toutes les valeurs d'un tableau de nombres.
 
 4. Essayez de faire une méthode qui remet à 0 la variable de classe $result
+
+5. Faite également la méthode divide qui divise deux nombres.x@
 */
 
 $calculatrice = new Calcul();
 
-$calculatrice->add(1,2);
+$calculatrice->add(1, 2);
 echo $calculatrice->getResult();
 echo PHP_EOL;
 
-echo $calculatrice->sum([1,2,3,4,5]);
+echo $calculatrice->sum([1, 2, 3, 4, 5]);
+echo $calculatrice->getResult();
+echo PHP_EOL;
+
+$calculatrice->reset();
+
+echo $calculatrice->mults([1, 2, 3, 4, 5]);
+echo $calculatrice->getResult();
+echo PHP_EOL;
+
+$calculatrice->reset();
+
+echo $calculatrice->divide(1,10);
 echo $calculatrice->getResult();
 echo PHP_EOL;
