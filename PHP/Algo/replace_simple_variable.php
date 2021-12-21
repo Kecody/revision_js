d05132ce2f1cb5aa985054b3efe171ad28f350ca
Antoine07 <antoine.lucsko@gmail.com>
2021-12-21 11:17:56 +0100

    sommes

--- PHP/Algo/replace_simple_variable.php
+++ PHP/Algo/replace_simple_variable.php
@@ -6,5 +6,21 @@
  * Remplacez la valeur $a par la valeur $b et la valeur $b par la valeur $a à l'aide d'un script
  */
 
- $a = 10;
- $b = 11;
+$a = 10;
+$b = 11;
+echo "a : $a";
+echo PHP_EOL;
+echo "b : $b";
+echo PHP_EOL;
+$c = $a;
+$a = $b;
+$b = $c;
+echo PHP_EOL;
+echo "a : $a";
+echo PHP_EOL;
+echo "b : $b";
+// attention simple cote n'évalue pas les variables dans la chaîne de caractères.
+// echo PHP_EOL;
+// echo 'b : $b';
+
+echo PHP_EOL;
---
+++ PHP/Algo/somme.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Exercice
+
+Faite la somme des deux tableaux suivants terme à terme
+
+$number1 = [1,2,3,4, 5, 6, 7, 8, 9, 10];
+$number2 = [10, 20, 30,40, 50, 60, 70, 80, 90, 100];
+
+*/
