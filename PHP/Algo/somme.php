6956abe1639e0a328cd7279352e710dd64a76809
Antoine07 <antoine.lucsko@gmail.com>
2021-12-21 11:57:44 +0100

    debut exo

--- PHP/Algo/somme.php
+++ PHP/Algo/somme.php
@@ -9,3 +9,15 @@ $number1 = [1,2,3,4, 5, 6, 7, 8, 9, 10];
 $number2 = [10, 20, 30,40, 50, 60, 70, 80, 90, 100];
 // 11, 22, 33, 44, ... 
 */
+
+$number1 = [1,2,3,4, 5, 6, 7, 8, 9, 10];
+$number2 = [10, 20, 30,40, 50, 60, 70, 80, 90, 100];
+
+// faire la somme des deux tableaux que si ils sont de même longueur 
+if(count($number1) == count($number2)){
+    $dim = count($number1) ;
+
+    for($key=0; $key < $dim ; $key++){
+        
+    }
+}
