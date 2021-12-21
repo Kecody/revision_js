e99384de12e9af4345a943fd97b1f1c0efd552b4
Antoine07 <antoine.lucsko@gmail.com>
2021-12-21 13:28:59 +0100

    numbers add

--- PHP/Algo/somme.php
+++ PHP/Algo/somme.php
@@ -12,12 +12,15 @@ $number2 = [10, 20, 30,40, 50, 60, 70, 80, 90, 100];
 
 $number1 = [1,2,3,4, 5, 6, 7, 8, 9, 10];
 $number2 = [10, 20, 30,40, 50, 60, 70, 80, 90, 100];
-
+$total = [];
 // faire la somme des deux tableaux que si ils sont de même longueur 
 if(count($number1) == count($number2)){
     $dim = count($number1) ;
 
     for($key=0; $key < $dim ; $key++){
-        
+        $total[] = $number1[$key] + $number2[$key] ;
     }
 }
+
+echo print_r($total);
+echo PHP_EOL;
