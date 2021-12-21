820ebf07847bebefe58682101e75360053abce52
Antoine07 <antoine.lucsko@gmail.com>
2021-12-21 13:35:49 +0100

    lancer de de

---
+++ PHP/Algo/de.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Exercice 
+ *  1. On jete un dé à 6 faces équilibrés. Créez une fonction qui simule un lancer.
+ * 
+ *  2. Répétez 100000 fois un lancer de dé et calculez la probabilité d'obtenir un 3 
+ */
+
+ // 1
+ function laner(int $numFaces = 6){
+
+ }
+
+ laner(); // 1, 2, 3, 4,..., 6
+
+ // 2
+ $count = 0 ; // comptez le nombre de fois que vous obtenez 3
+
+
+// Affichez la proba
+
