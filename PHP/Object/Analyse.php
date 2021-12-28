29d3de14411c21c6c8220e350d435c72b6c51aeb
Antoine07 <antoine.lucsko@gmail.com>
2021-12-28 11:51:08 +0100

    titanic

--- PHP/Object/Analyse.php
+++ PHP/Object/Analyse.php
@@ -1,18 +1,18 @@
 <?php
 
 $file =  "./Data/titanic.csv" ;
-$handle = fopen($file, "r"); // ouvre une ressource avec l'adresse du fichier
+// $handle = fopen($file, "r"); // ouvre une ressource avec l'adresse du fichier
 
-// tant que je n'arrive pas à la fin du fichier je boucle
-while(!feof($handle)){
-    // lire les données ligne par ligne
-    $content =  fgets($handle);
-    print_r(explode(",", $content));
-    echo PHP_EOL ;
-    break;
-}
+// // tant que je n'arrive pas à la fin du fichier je boucle
+// while(!feof($handle)){
+//     // lire les données ligne par ligne
+//     $content =  fgets($handle);
+//     print_r(explode(",", $content));
+//     echo PHP_EOL ;
+//     break;
+// }
 
-fclose($handle); // on ferme le fichier lu
+// fclose($handle); // on ferme le fichier lu
 
 // Pour lire un fichier csv avec la fonction fgetcsv de PHP
 
@@ -20,8 +20,18 @@ fclose($handle); // on ferme le fichier lu
 $handle = fopen($file, "r"); 
 
 // l'assignation se fait avant l'évaluation dans le while pour arrêter la boucle
+
+// je récupère la première ligne lecture ligne par ligne, à la fin il retourne false quand fgetcsv n'a plus rien à lire 
+$data = fgetcsv($handle, 1000, ",");
+print_r($data);
+$survived = 0;
 while( $data = fgetcsv($handle, 1000, ",") ){
-    print_r($data);
+    $survived += $data[1];
 }
 
+echo "survived $survived";
+echo PHP_EOL ;
 fclose($handle);
+
+// Exercice 01 
+// Comptez le nombre de survivant sur le titanic sachant que survivre 1 et ne pas survivre c'est 0
