7ba80783f02c9b207c412fcb1cd570bd64639b97
Antoine07 <antoine.lucsko@gmail.com>
2021-12-28 11:31:45 +0100

    read data

--- PHP/Object/Analyse.php
+++ PHP/Object/Analyse.php
@@ -1,13 +1,27 @@
 <?php
 
 $file =  "./Data/titanic.csv" ;
-$handle = fopen($file, "r"); // ouvre une ressource 
+$handle = fopen($file, "r"); // ouvre une ressource avec l'adresse du fichier
 
-// tant que je n'arrive pas à la fin du fichier
+// tant que je n'arrive pas à la fin du fichier je boucle
 while(!feof($handle)){
     // lire les données ligne par ligne
-    echo fgets($handle);
+    $content =  fgets($handle);
+    print_r(explode(",", $content));
     echo PHP_EOL ;
+    break;
 }
 
 fclose($handle); // on ferme le fichier lu
+
+// Pour lire un fichier csv avec la fonction fgetcsv de PHP
+
+// ré-ouvre la ressource
+$handle = fopen($file, "r"); 
+
+// l'assignation se fait avant l'évaluation dans le while pour arrêter la boucle
+while( $data = fgetcsv($handle, 1000, ",") ){
+    print_r($data);
+}
+
+fclose($handle);
