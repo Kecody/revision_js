721ecbfca379ce06615aec3989ce4f1a767afd52
Antoine07 <antoine.lucsko@gmail.com>
2021-12-28 21:51:04 +0100

    correction dataset

--- PHP/Object/Analyse_age.php
+++ PHP/Object/Analyse_age.php
@@ -10,6 +10,12 @@ while( $data = fgetcsv($ressource, 1000, ",") ){
 }
 
 // 01 Exercice 
-
 // Affichez l'age des passagés
+$ressource = fopen($file, "r"); 
+$data = fgetcsv($ressource, 1000, ",");
 
+while( $data = fgetcsv($ressource, 1000, ",") ){
+    print_r($data[5]);
+}
+// 02 Exercice 
+// Faire la somme de tous les ages et calculer la moyenne des ages
