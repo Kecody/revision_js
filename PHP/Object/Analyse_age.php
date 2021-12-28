c94ad342fd9069f42685cf60e89940c1098ca0d7
Kevin.Q <k.queyroi@gmail.com>
2021-12-28 21:58:15 +0100

    modif analyse age

--- PHP/Object/Analyse_age.php
+++ PHP/Object/Analyse_age.php
@@ -5,8 +5,12 @@ $file =  "./Data/titanic.csv" ;
 // ré-ouvre la ressource
 $ressource = fopen($file, "r"); 
 
+$data = fgetcsv($ressource, 1000, ",");
+print_r($data[5]);
+die;
 while( $data = fgetcsv($ressource, 1000, ",") ){
-    print_r($data);
+    print_r($data[5]);
+    echo PHP_EOL;
 }
 
 // 01 Exercice 
